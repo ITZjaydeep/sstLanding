cbeb7a55918a24cc61eb75ba7dd732343a8d4880
jaydeep agravat <jaydeep.agravat.silversky@gmail.com>
2025-10-31 12:06:14 +0530

    setup react-router-dom with @ path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <div></div>;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
+import Services from "@/pages/Services";
+import Team from "@/pages/Team";
+import Portfolio from "@/pages/Portfolio";
+import Blog from "@/pages/Blog";
+import Careers from "@/pages/Careers";
+import ContactUs from "@/pages/ContactUs";
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div>sdf</div>;
+}
+
+export default Home;
